af298e212eaeadd10e37fd96d9db318416a6ce6c
David Flores <31556253+mrdavis-dev@users.noreply.github.com>
2020-12-14 14:40:46 -0500

    footer

--- admin/modulos/footer.php
+++ admin/modulos/footer.php
@@ -1,5 +1,5 @@
 <footer>
 <small id="" class="form-text text-muted">Este sistema es creado por <a href="https://mrdavis-dev.github.io/">mrdavis-dev</a>.</small><br>
-<p>DEVELGEEK no se hace responsable por los datos introducidos en el sistema, ni tendra relación con los datos. los datos guardados son de suma privacidad y solo seran usados
+<p>mrdavis-dev no se hace responsable por los datos introducidos en el sistema, ni tendra relación con los datos. los datos guardados son de suma privacidad y solo seran usados
 por el equipo de trasabilidad.</p>
 </footer>
